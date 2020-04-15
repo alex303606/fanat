297aae99d04ba50cf8831aea6a4fe2ccb2ca69be
Aleksey Ostrikov <alex303606@gmail.com>
2020-04-15 14:02:32 +0600

    added qr code scanner

--- src/screens/SecondScreen.jsx
+++ src/screens/SecondScreen.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import ScreenWrapper from './ScreenWrapper';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import Button from '../components/Button';
 import { useNavigation } from '@react-navigation/native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera as Camera } from 'react-native-camera';
 const styles = EStyleSheet.create({});
 
 const SecondScreen = () => {
 	const navigation = useNavigation();
-	
+	const onSuccess = e => {
+		alert(e.data);
+	};
 	return (
 		<ScreenWrapper>
-			<Text style={{
-				color: 'white',
-				fontSize: 30,
-				fontWeight: 'bold',
-			}}>SecondScreen</Text>
-			<Button
-				onPress={() => {
-					navigation.navigate('First');
-				}}
-				title={'go to First'}
+			<QRCodeScanner
+				fadeIn={false}
+				onRead={onSuccess}
+				flashMode={Camera.Constants.FlashMode.auto}
+				topContent={
+					<Text style={styles.centerText}>
+						Go to{' '}
+						<Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+						your computer and scan the QR code.
+					</Text>
+				}
+				bottomContent={
+					<TouchableOpacity style={styles.buttonTouchable}>
+						<Text style={styles.buttonText}>OK. Got it!</Text>
+					</TouchableOpacity>
+				}
 			/>
 		</ScreenWrapper>
 	);
